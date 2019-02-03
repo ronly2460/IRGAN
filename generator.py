22f61c0e944b44a5856b02cef0ab63bb5abe8fb7
Takuma Nishimura <takuma2460.0131@gmail.com>
2019-02-03 19:29:55 +0000

    fix generator

--- generator.py
+++ generator.py
@@ -3,9 +3,11 @@ import torch.nn.functional as F
 import torch
 import torchvision.models as models
 
-def Generator():
-    def __init__(self, item_num, user_num, 
-                            emb_dim, lamda, param=None, initdelta=0.05, lr=0.05):
+class Generator(nn.Module):
+    
+    def __init__(self, item_num, user_num, emb_dim,
+                           lamda, param=None, initdelta=0.05, lr=0.05):
+        super(Generator, self).__init__()
         self.item_num = item_num
         self.user_num = user_num
         self.emb_dim = emb_dim
@@ -16,22 +18,35 @@ def Generator():
         self.g_params = []
         
         if self.param is None:
-            self.user_embeddings = nn.Embedding() # (self.user_num, self.emb_dim)
-            self.item_embeddings = nn.Embedding() # (self.item_num, self.emb_dim)
-            self.item_bias = torch.zeros([], dtype=) # (self.item_num)
+            self.user_embeddings = nn.Embedding(
+                self.user_num, self.emb_dim)
+            self.item_embeddings = nn.Embedding(
+                self.item_num, self.emb_dim)
+            self.item_bias = torch.zeros(self.item_num) # (self.item_num)
+            
+            # initialize
+            torch.nn.init.uniform_(
+                self.user_embeddings.weight, a=initdelta, b=-initdelta)
+            torch.nn.init.uniform_(
+                self.item_embeddings.weight, a=initdelta, b=-initdelta)
         else:
             # parameterの読み込み
+            print("aaa")
         
         def forward(self, user, item, label, reward):
             i_prob = torch.gather(F.softmax(self.all_logits(user).view(1, -1), -1), item)
-            loss = - (torch.log(i_prob) * reward).sum(1)
-                       + self.lamda * (F.normalize(self.user_embeddings(user), p=2, dim=1)
-                                                 + F.normalize(self.item_embeddings(item), p=2, dim=1)
-                                                 + F.normalize(self.item_bias(item), p=2, dim=1)
+            loss = - (torch.log(i_prob) * reward).sum(1) \
+                       + self.lamda * (F.normalize(self.user_embeddings(user), p=2, dim=1) \
+                                                 + F.normalize(self.item_embeddings(item), p=2, dim=1) \
+                                                 + F.normalize(self.item_bias(item), p=2, dim=1))
             return loss
-            
+
+        def all_rating(self, user):
+            all_rating = torch.mm(self.user_embeddings(user),
+                                                   self.item_embeddings.t()) + self.item_bias
+            return all_rating
+                                       
         def all_logits(self, user):
-            u_embedding = self.user_embeddings(user)
             all_logits = (self.user_embeddings(user) \
                                   * self.item_embeddings).sum(1) + self.item_bias
             return all_logits
