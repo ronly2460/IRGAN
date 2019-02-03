8c8f9a8aaa93770c7dda2425d49ffdd285099b58
Takuma Nishimura <takuma2460.0131@gmail.com>
2019-02-03 19:29:39 +0000

    fix descriminator

--- discriminator.py
+++ discriminator.py
@@ -3,25 +3,35 @@ import torch.nn.functional as F
 import torch
 import torchvision.models as models
 
-class Discriminator():
-    def __init__(self, item_num, user_num, 
-                            emb_dim, lamda, param=None, initdelta=0.05, lr=0.05):
+class Discriminator(nn.Module):
+    
+    def __init__(self, item_num, user_num, emb_dim,
+                           lamda, param=None, initdelta=0.05):
+        super(Discriminator, self).__init__()
         self.item_num = item_num
         self.user_num = user_num
         self.emb_dim = emb_dim
         self.lamda = lamda
         self.param = param
         self.initdelta = initdelta
-        self.lr = lr
         self.d_params = []
         
         if self.param is None:
-            self.user_embeddings = nn.Embedding() # (self.user_num, self.emb_dim)
-            self.item_embeddings = nn.Embedding() # (self.item_num, self.emb_dim)
-            self.item_bias = torch.zeros([], dtype=) # (self.item_num)
+            self.user_embeddings = nn.Embedding(
+                self.user_num, self.emb_dim)
+            self.item_embeddings = nn.Embedding(
+                self.item_num, self.emb_dim)
+            self.item_bias = torch.zeros(self.item_num) # (self.item_num)
+            
+            # initialize
+            torch.nn.init.uniform_(
+                self.user_embeddings.weight, a=initdelta, b=-initdelta)
+            torch.nn.init.uniform_(
+                self.item_embeddings.weight, a=initdelta, b=-initdelta)
         else:
             # parameterの読み込み
-        
+            print("aaa")
+            
         def forward(self, user, item, label):
             pre_logits = (self.user_embeddings(user) \
                                    * self.item_embeddings(item)).sum(1) + self.item_bias(item)
@@ -41,6 +51,6 @@ class Discriminator():
         
         def get_reward(self, user, item):
             reward_logits = (self.user_embeddings(user) \
-                                         * self.item_embeddings.t()).sum(1) + self.item_bias(item)
+                                         * self.item_embeddings(item).t()).sum(1) + self.item_bias(item)
             reward = 2 * (torch.nn.Sigmoid(reward_logits) - 0.5)
             return reward
