ec42f26cfe49b1d72c44f279e3648c21a53b8965
Takuma Nishimura <takuma2460.0131@gmail.com>
2019-02-05 20:05:39 +0000

    fix discriminator

--- discriminator.py
+++ discriminator.py
@@ -1,8 +1,3 @@
-import torch.nn as nn
-import torch.nn.functional as F
-import torch
-import torchvision.models as models
-
 class Discriminator(nn.Module):
     
     def __init__(self, item_num, user_num, emb_dim,
@@ -18,39 +13,38 @@ class Discriminator(nn.Module):
         
         if self.param is None:
             self.user_embeddings = nn.Embedding(
-                self.user_num, self.emb_dim)
+                self.user_num, self.emb_dim)            
             self.item_embeddings = nn.Embedding(
-                self.item_num, self.emb_dim)
-            self.item_bias = torch.zeros(self.item_num) # (self.item_num)
+                self.item_num, self.emb_dim)            
+            self.item_bias = nn.Embedding(
+                self.item_num, 1)
             
             # initialize
             torch.nn.init.uniform_(
                 self.user_embeddings.weight, a=initdelta, b=-initdelta)
             torch.nn.init.uniform_(
                 self.item_embeddings.weight, a=initdelta, b=-initdelta)
-        else:
-            # parameterの読み込み
-            print("aaa")
+#             torch.nn.init.uniform_(
+#                 self.item_bias.weight, a=initdelta, b=-initdelta)
             
-        def forward(self, user, item, label):
-            pre_logits = (self.user_embeddings(user) \
-                                   * self.item_embeddings(item)).sum(1) + self.item_bias(item)
-            pre_loss =  F.binary_cross_entropy_with_logits(pre_logits, label)
-            return pre_loss
+    def forward(self, user, item, label):
+        pre_logits = (self.user_embeddings(user) \
+                              * self.item_embeddings(item)).squeeze().sum(1).view(-1,1) \
+                                + self.item_bias(item).view(-1, 1)
+        pre_loss =  F.binary_cross_entropy_with_logits(pre_logits, label)
+        return pre_loss
 
-        def all_rating(self, user):
-            all_rating = torch.mm(self.user_embeddings(user),
-                                                   self.item_embeddings.t()) + self.item_bias
-            return all_rating
-            
-        def all_logits(self, user):
-            u_embedding = self.user_embeddings(user)
-            all_logits = (self.user_embeddings(user) \
-                                  * self.item_embeddings).sum(1) + self.item_bias
-            return all_logits
-        
-        def get_reward(self, user, item):
-            reward_logits = (self.user_embeddings(user) \
-                                         * self.item_embeddings(item).t()).sum(1) + self.item_bias(item)
-            reward = 2 * (torch.nn.Sigmoid(reward_logits) - 0.5)
-            return reward
+    def all_rating(self, user):
+        rating = torch.mm(self.user_embeddings(user).view(1, -1),
+                                               self.item_embeddings.weight.t()) + self.item_bias
+        return all_rating
+
+    def all_logits(self, user):
+        logits = (self.user_embeddings(user) \
+                              * self.item_embeddings.weight).sum(1) + self.item_bias
+        return all_logits
+
+    def get_reward(self, user, item):
+        reward_logits = (self.user_embeddings(user) * self.item_embeddings(item)).squeeze().sum(1).view(-1,1) + self.item_bias(item).view(-1, 1)
+        reward = 2 * (torch.sigmoid(reward_logits) - 0.5)
+        return reward
